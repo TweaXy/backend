b9d84c66051d9e5db0363fc63b0adfb70d368d9c
aliaa gheis <aliaagheis@gmail.com>
2023-10-25 00:19:44 +0300

    [ ➕ FR ] add forget password validation schema; [ 📝 DOC ] add forget password api documentation;

--- src/validations/authSchema.js
+++ src/validations/authSchema.js
@@ -4,8 +4,28 @@ import yup from 'yup';
 
 const sendEmailVerificationSchema = yup.object({
     body: yup.object({
-        email: yup.string().email().required(),
+        email: yup.string().email().required('email is required field'),
     }),
 });
 
-export { sendEmailVerificationSchema };
+const forgetPasswordSchema = yup.object({
+    body: yup.object({
+        uniqueIdentifier: yup
+            .mixed()
+            .oneOf([
+                yup.string().email('email must be a valid email address'),
+                yup
+                    .string()
+                    .matches(/^[0-9]+$/, 'phone must be a number')
+                    .min(11, 'phone must be 11 digits')
+                    .max(11, 'phone must be 11 digits'),
+                yup
+                    .string()
+                    .min(4, 'username must be at least 4 characters')
+                    .max(191, 'username must be at most 191'),
+            ])
+            .required('email or phone or username is required field '),
+    }),
+});
+
+export { sendEmailVerificationSchema, forgetPasswordSchema };
