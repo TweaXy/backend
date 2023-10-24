2aa36c10839f861fdad607db15b7da19798d273e
Sara Gamal <sarah.gerges01@eng-st.cu.edu.eg>
2023-10-24 20:44:56 +0300

    [ ➕ FR ] Adding login Page;

--- src/services/userService.js
+++ src/services/userService.js
@@ -50,5 +50,84 @@ const GetUserById = async (id) => {
         },
     });
 };
+const addToken = async (id, token) => {
 
-export default { GetAllUsers, GetUserByEmail, GetUserById };
+    const createdToken = await prisma.tokens.create({
+        data: {
+            userID: id,
+            token,
+        },
+    });
+
+    await prisma.user.update({
+        where: {
+            id
+        },
+        data: {
+            tokens: {
+                     id: createdToken.id ,
+            },
+        },
+});
+};
+
+
+/**
+ * Retrieves user by email .
+ * @async
+ * @method
+ * @param {String} email- User email
+ * * @param {String} password- User password
+ * @returns {User} User object
+ * @throws {NotFoundError} When the user is not found.
+ */
+const FindUserByEmail = async (email,password) => {
+    return await prisma.user.findUnique({
+        where: {
+            email: email,
+            password:password,
+        },
+    });
+};
+
+
+
+/**
+ * Retrieves user by email .
+ * @async
+ * @method
+ * @param {number} phone- User phone number
+ * * @param {String} password- User password
+ * @returns {User} User object
+ * @throws {NotFoundError} When the user is not found.
+ */
+const FindUserByPhone = async (phone,password) => {
+    return await prisma.user.findFirst({
+        where: {
+            phone: phone,
+            password:password,
+        },
+    });
+};
+
+
+
+/**
+ * Retrieves user by email .
+ * @async
+ * @method
+ * @param {String} username- Username
+ * * @param {String} password- User password
+ * @returns {User} User object
+ * @throws {NotFoundError} When the user is not found.
+ */
+const FindUserByUsername = async (username,password) => {
+    return await prisma.user.findUnique({
+        where: {
+            username: username,
+            password:password,
+        },
+    });
+};
+
+export default { GetAllUsers, GetUserByEmail, GetUserById ,addToken,FindUserByEmail,FindUserByPhone,FindUserByUsername};
