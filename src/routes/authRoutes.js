418daf51eb4a578793cc290e0ceeeb8ada5d38ea
aliaa gheis <aliaagheis@gmail.com>
2023-10-25 00:19:44 +0300

    [ ➕ FR ] add forget password validation schema; [ 📝 DOC ] add forget password api documentation;

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -108,10 +108,116 @@ import authController from '../controllers/authController.js';
  *                 message: 'Internal Server Error'
  */
 
+/**
+ * @swagger
+ * /auth/forgetPassword:
+ *   post:
+ *     summary: Send an email to a user with token and url to reset password
+ *     tags: [Auth]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             required:
+ *               - uniqueIdentifier
+ *             properties:
+ *               uniqueIdentifier:
+ *                 type: string
+ *                 description: an email or username or phone.
+ *                 format: email | username | phone
+ *             example:
+ *               uniqueIdentifier: "aliaa@aliaa.com"
+ *     responses:
+ *       200:
+ *         description: Email with token sent successfully.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 status:
+ *                   type: string
+ *                   enum: [success]
+ *                 data:
+ *                   type: object
+ *                   description: null
+ *               example:
+ *                 status: success
+ *                 data: null
+ *       404:
+ *         description: Not found - no user exist with (username | email | phone).
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 status:
+ *                   type: string
+ *                   enum: [fail]
+ *                   description: The status of the response.
+ *                 message:
+ *                   type: string
+ *                   enum: [Cannont find user with following data.]
+ *       403:
+ *         description: Forbidden Request - validation fail.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 status:
+ *                   type: string
+ *                   enum: [fail]
+ *                   description: The status of the response.
+ *                 message:
+ *                   type: string
+ *               example:
+ *                  status: fail
+ *                  message: 'email or phone or username is required field'
+ *       429:
+ *         description: More than one request in less than 30 seconds
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 status:
+ *                   type: string
+ *                   enum: [fail]
+ *                 message:
+ *                   type: string
+ *                   description: the error message
+ *                   enum: [More than one request in less than 30 seconds]
+ *       500:
+ *         description: Internal Server Error - Something went wrong on the server.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 status:
+ *                   type: string
+ *                   enum: [error]
+ *                   description: The status of the response.
+ *                 message:
+ *                   type: string
+ *                   description: A general error message.
+ *               example:
+ *                 status: 'error'
+ *                 message: 'Internal Server Error'
+ */
+
 const authRouter = Router();
 
 authRouter.post(
     '/sendEmailVerification',
+    validateMiddleware(sendEmailVerificationSchema),
+    authController.SendEmailVerification
+);
+
+authRouter.post(
+    '/gorgetPassword',
     validateMiddleware(sendEmailVerificationSchema),
     authController.SendEmailVerification
 );
