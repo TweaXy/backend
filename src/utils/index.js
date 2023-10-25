6288b46e66d86b6df6b9082bd8066070cdf7b5f9
aliaa gheis <aliaagheis@gmail.com>
2023-10-26 01:56:58 +0300

    [ 🎨 REF ] create utils index file; clean login, signup

--- src/utils/index.js
+++ src/utils/index.js
@@ -2,3 +2,5 @@ export { default as catchAsync } from './catchAsync.js';
 export { default as createRandomByteToken } from './createRandomByteToken.js';
 export { default as isUUID } from './isUUID.js';
 export { sendVerificationEmail, sendForgetPasswordEmail } from './sendEmail.js';
+export { default as addAvatar } from './addAvatar.js';
+export { default as generateToken } from './generateToken.js';
