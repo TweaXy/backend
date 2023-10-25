23ce80742ca0b28097d2da167030204bcdf1521c
Eman <ibrahim.eman83@gmail.com>
2023-10-25 08:57:06 +0300

    [ ➕ FR ] Adding signup;

--- src/utils/addAvatar.js
+++ src/utils/addAvatar.js
@@ -1,9 +1,19 @@
-import sharp from 'sharp'
+import sharp from 'sharp';
+import fs from 'fs';
+
 const AddAvatar = async (buffer) => {
 
-    const createdBuffer = await sharp(buffer).resize({ width: 250, height: 250 }).png().toBuffer();
+    let createdBuffer;
+    if (buffer)
+        createdBuffer = await sharp(buffer).resize({ width: 250, height: 250 }).png().toBuffer();
+    else
+    {
+        const defaultBuffer=await fs.promises.readFile('src/utils/default.png');
+        createdBuffer=await sharp(defaultBuffer).resize({ width: 250, height: 250 }).png().toBuffer();
+    }
+
     return createdBuffer;
 
-}
+};
 
 export default AddAvatar;
