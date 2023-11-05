261ee78192ca8ac5592102a8d6e00ea514e618e3
nesma-shafie <nesmashafie342@gmail.com>
2023-11-05 20:12:40 +0200

    [ ➕ FR ] new part of the DB

---
+++ prisma/migrations/20231103123535_default_trend_rank/migration.sql
@@ -0,0 +1,2 @@
+-- AlterTable
+ALTER TABLE `trends` MODIFY `rank` INTEGER NOT NULL DEFAULT 1;
--- prisma/seed.js
+++ prisma/seed.js
@@ -1,35 +1,120 @@
 import { PrismaClient } from '@prisma/client';
 import { faker } from '@faker-js/faker';
+import { init } from '@paralleldrive/cuid2';
 import Chance from 'chance';
 const chance = new Chance();
 
 
 const prisma = new PrismaClient();
+const createID = init({ length: 25 });
+const minNameLength = 4; // Change this to your desired minimum length
+const trendWords = new Set(); // Change this to your desired minimum length
 
+function generateRandomName() {
+    let name = faker.person.firstName();
+
+    while (name.length < minNameLength) {
+        // Regenerate a name until it meets the minimum length requirement
+        name = faker.person.firstName();
+    }
+
+    return name;
+}
+
+function generateUniqueWord() {
+    let condition = true;
+    while (condition) {
+        const word = faker.lorem.word({ length: { min: 5, max: 10 } });
+        if (!trendWords.has(word)) {
+            trendWords.add(word);
+            return word;
+        }
+    }
+}
 
 const main = async () => {
     console.log('Start seeding ...');
-
-
+    let usersIDS = [];
+    let trendsIDS = [];
+    let interactionsIDS = [];
+    /////////creating 10 users
     for (let i = 0; i < 10; i++) {
-        const person = faker.person;
+        let person = faker.person;
+        let newID = createID();
+        usersIDS.push(newID);
         await prisma.user.create({
             data: {
+                id: newID,
                 username: `${person.firstName()}_${person.lastName()}`,
-                name: person.firstName({}),
+                name: generateRandomName(),
                 password: faker.helpers.arrayElement([
-                    '$2a$08$jfdElvFY5OaiC7oBgTT9WOlRLJ2cNfRxj98GXAoFteGuXjyHxpO46',
-                    '$2a$08$wIyNnOdsMiNAU9orWqo1iO30mdQg/eIOfQqL44lF.IFSpAuCn8Yfq',
-                    '$2a$08$XM/JeCP6NJVF97wRfJ2hGu5dbw.3WyZkIlnla5Y29cBg8vQ3L5EJ6']),
+                    '$2a$08$ad.6THl.NHxdAYfgQIh5deg6YOtsfwTWvI7AM6II6jkgop05.n3SS',
+                    '$2a$08$/nTMIO0QQAYxXwOJ18IUBeWAeea2lb1aE6XhnIxBN96BA.xjucbai',
+                    '$2a$08$52kiBx2aGG898hg.VahyteFIC44a./VAz.B2zUz6AjqP/nggtcno6']),
                 email: `${person.firstName()}@gmail.com`,
                 phone: `0${chance.phone({ formatted: false })}`,
                 birthdayDate: faker.date.birthdate(),
                 location: faker.location.city(),
-                website: faker.internet.url(),
             }
         });
     }
-    console.log('Seeding finished.');
+
+    /////////creating 6 trends
+    for (let j = 0; j < 6; j++) {
+        let newTrend = createID();
+        trendsIDS.push(newTrend);
+        await prisma.trends.create({
+            data: {
+                id: newTrend,
+                text: `#${generateUniqueWord()}`,
+                rank: 5
+            }
+        });
+    }
+    /////////creating 2 Tweets for each user
+    for (let i = 0; i < 10; i++) {
+
+        for (let j = 0; j < 3; j++) {
+            let newID = createID();
+            interactionsIDS.push(newID);
+            await prisma.interactions.create({
+                data: {
+                    id: newID,
+                    type: 'T',
+                    text: faker.lorem.sentence(),
+                    userID: usersIDS[i]
+                }
+            });
+        }
+    }
+
+    /////////put in each iinteraction trend
+    let trentNumber = 0;
+    for (let i = 0; i < 30; i++) {
+        await prisma.trendsInteractions.create({
+            data: {
+                trendID: trendsIDS[trentNumber],
+                interactionID: interactionsIDS[i]
+            }
+        });
+        trentNumber++;
+        if (trentNumber == 6) trentNumber = 0;
+    }
+
+    /////////put in each user 3following
+    for (let i = 0; i < 5; i++) {
+        for (let j = 1; j <= 3; j++) {
+
+            await prisma.follow.create({
+                data: {
+                    userID: usersIDS[i],
+                    followingUserID: usersIDS[i + j]
+                }
+            });
+        }
+    }
+
+    console.log('finish seeding ...');
 };
 
 main()
