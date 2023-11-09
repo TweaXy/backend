5609cb3e8541e6df73ba97f29c3e60b318c90b18
aliaa gheis <aliaagheis@gmail.com>
2023-11-10 01:32:58 +0200

    [🐞 BUG] fix non hosting media files;

--- src/app.js
+++ src/app.js
@@ -16,17 +16,23 @@ import cookieParser from 'cookie-parser';
 
 import passport from 'passport';
 
-
 import cors from 'cors';
+
+import path from 'path';
+import { fileURLToPath } from 'url';
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 // config swagger
 const swaggerSpecs = swaggerJsdoc(swaggerConfig);
 const app = express();
+
 app.use(cors());
 app.use(cookieParser());
-
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+app.use('/public', express.static(path.join(__dirname, '../uploads')));
 // for logging in dev environment
 if (process.env.NODE_ENV === 'development') {
     app.use(morgan('dev'));
