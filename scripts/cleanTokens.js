690bd68fbf391e3c0f4dbcf4e12bd83115eb641b
aliaa gheis <aliaagheis@gmail.com>
2023-11-10 01:12:53 +0200

    [ ➕ FR ] add expiredToken to DB; add service to delete expired data

---
+++ scripts/cleanTokens.js
@@ -0,0 +1,1 @@
+import crons from 'node-cron';
--- src/server.js
+++ src/server.js
@@ -1,11 +1,12 @@
 import app from './app.js';
 import dotenv from 'dotenv';
+import cron from 'node-cron';
 
 dotenv.config({ path: './.env' });
 
 const PORT = process.env.PORT || 3000;
 
-const server = app.listen(PORT, (err) => { 
+const server = app.listen(PORT, (err) => {
     if (err) {
         console.error(`Error: ${err}`);
     } else {
