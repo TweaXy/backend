8a445a3bd8ed21108784cd0f1990951bb5c5fdb9
aliaa gheis <aliaagheis@gmail.com>
2023-11-12 18:36:01 +0200

    [🐞 BUG] add try catch around crons and prisma middleware

--- src/prisma.js
+++ src/prisma.js
@@ -3,31 +3,39 @@ import { PrismaClient } from '@prisma/client';
 const prisma = new PrismaClient();
 
 prisma.$use(async (params, next) => {
-    if (
-        params.model == 'Trends' ||
-        params.model == 'Interactions' ||
-        params.model == 'User' ||
-        params.model == 'DirectMessages'
-    ) {
-        if (params.action == 'delete' || params.action == 'deleteMany') {
-            params.action =
-                params.action === 'delete' ? 'update' : 'updateMany';
-
-            if (params.args.data)
-                params.args.data['deletedDate'] = new Date().toISOString();
-            else params.args.data = { deletedDate: new Date().toISOString() };
-        }
+    try {
         if (
-            params.action == 'findFirst' ||
-            params.action == 'findUnique' ||
-            params.action == 'findMany' ||
-            params.action == 'update' ||
-            params.action == 'updateMany'
+            params.model == 'Trends' ||
+            params.model == 'Interactions' ||
+            params.model == 'User' ||
+            params.model == 'DirectMessages'
         ) {
-            if (params.args.where) params.args.where['deletedDate'] = null;
-            else params.args.where = { deletedDate: null };
+            if (params.action == 'delete' || params.action == 'deleteMany') {
+                params.action =
+                    params.action === 'delete' ? 'update' : 'updateMany';
+
+                if (params.args.data)
+                    params.args.data['deletedDate'] = new Date().toISOString();
+                else
+                    params.args.data = {
+                        deletedDate: new Date().toISOString(),
+                    };
+            }
+            if (
+                params.action == 'findFirst' ||
+                params.action == 'findUnique' ||
+                params.action == 'findMany' ||
+                params.action == 'update' ||
+                params.action == 'updateMany'
+            ) {
+                if (params.args.where) params.args.where['deletedDate'] = null;
+                else params.args.where = { deletedDate: null };
+            }
         }
+        return next(params);
+    } catch (error) {
+        console.error('Error in prisma middleware 🤯: ', error);
+        return next(params);
     }
-    return next(params);
 });
 export default prisma;
