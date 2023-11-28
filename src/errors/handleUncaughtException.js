2badaba99a4d7fa261ab1dc179df30b91e0d0dc1
aliaa gheis <aliaagheis@gmail.com>
2023-11-28 08:24:09 +0200

    [:hammer:] add error loggin when something wrong happen

--- src/errors/handleUncaughtException.js
+++ src/errors/handleUncaughtException.js
@@ -1,7 +1,16 @@
+import { sendErrorLogEmail } from '../utils/sendEmail.js';
+
 // handle synchronus uncaught exceptions
-process.on('uncaughtException', (err) => {
-    console.log('UNCAUGHT EXCEPTION! 💥 Shutting down...');
-    console.log(err.name, err.message);
-    process.exit(1);
+process.on('uncaughtException', async (err) => {
+    try {
+        console.log('UNCAUGHT EXCEPTION! 💥 Shutting down...');
+        console.log({ name: err.name, message: err.message, stack: err.stack });
+
+        // Send email asynchronously without blocking the process
+        await sendErrorLogEmail(err);
+    } finally {
+        // Perform any necessary cleanup before exiting
+        process.exit(1);
+    }
     //TODO: restart server after 10 seconds
 });
