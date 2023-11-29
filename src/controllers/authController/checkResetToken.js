ce2e9c5d91f1b52c4dcde5b798891dc160a27652
aliaa gheis <aliaagheis@gmail.com>
2023-11-29 21:56:26 +0200

    [🐞 BUG] hotfix add await to asyn handleWrongResetToken

--- src/controllers/authController/checkResetToken.js
+++ src/controllers/authController/checkResetToken.js
@@ -3,7 +3,7 @@ import { catchAsync, handleWrongResetToken } from '../../utils/index.js';
 const checkResetToken = catchAsync(async (req, res, next) => {
     const { email, token } = req.params;
 
-    handleWrongResetToken(email, token);
+    await handleWrongResetToken(email, token);
 
     return res.status(200).json({
         status: 'success',
