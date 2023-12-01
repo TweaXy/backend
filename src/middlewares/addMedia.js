da726caf51a191d365c50628be3c1653eb8813e1
Sara Gamal <sarah.gerges01@eng-st.cu.edu.eg>
2023-12-01 23:17:44 +0200

    [🎨 REF] adding videos in interactions

--- src/middlewares/addMedia.js
+++ src/middlewares/addMedia.js
@@ -4,16 +4,20 @@ const storage = multer.diskStorage({
     destination: 'uploads/tweetsMedia/',
     filename: (req, file, cb) => {
         // Generate a unique filename based on username and date
-        const uniqueFilename = file.originalname + '_' + Date.now() + '.png';
+        const uniqueFilename = Date.now() + file.originalname;
         cb(null, uniqueFilename);
     },
 });
 const upload = multer({
     storage: storage,
-    limits: { fileSize: 2000000 },
+    limits: { fileSize: 10000000 },
     fileFilter(req, file, cb) {
-        if (!file.originalname.match(/\.(jpg|jpeg|png)$/)) {
-            return cb(new Error('please upload an image'));
+        if (
+            !file.originalname
+                .toLowerCase()
+                .match(/\.(jpg|jpeg|png|mp4|mkv|mov)$/)
+        ) {
+            return cb(new Error('please upload an image or video'));
         }
         cb(undefined, true);
     },
