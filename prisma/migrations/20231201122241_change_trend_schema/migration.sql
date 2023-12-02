4bf36c95c390353426b414fcf5c0bf4a834219c7
aliaa gheis <aliaagheis@gmail.com>
2023-12-02 16:03:47 +0200

    [:hammer:] fix migration

--- prisma/migrations/20231201122241_change_trend_schema/migration.sql
+++ prisma/migrations/20231201122241_change_trend_schema/migration.sql
@@ -1,54 +1,54 @@
 /*
   Warnings:
 
-  - The primary key for the `blocks` table will be changed. If it partially fails, the table could be left without primary key constraint.
-  - You are about to drop the column `UserID` on the `blocks` table. All the data in the column will be lost.
-  - You are about to drop the column `User1ID` on the `conversations` table. All the data in the column will be lost.
-  - You are about to drop the column `User2ID` on the `conversations` table. All the data in the column will be lost.
-  - The primary key for the `follow` table will be changed. If it partially fails, the table could be left without primary key constraint.
-  - You are about to drop the column `FollowingUserID` on the `follow` table. All the data in the column will be lost.
-  - You are about to drop the column `UserID` on the `follow` table. All the data in the column will be lost.
-  - You are about to drop the column `UserID` on the `interactions` table. All the data in the column will be lost.
-  - The primary key for the `likes` table will be changed. If it partially fails, the table could be left without primary key constraint.
-  - You are about to drop the column `UserID` on the `likes` table. All the data in the column will be lost.
-  - You are about to drop the column `DirectMessagesId` on the `media` table. All the data in the column will be lost.
-  - You are about to drop the column `InteractionsID` on the `media` table. All the data in the column will be lost.
-  - The primary key for the `mentions` table will be changed. If it partially fails, the table could be left without primary key constraint.
-  - You are about to drop the column `UserID` on the `mentions` table. All the data in the column will be lost.
-  - The primary key for the `mutes` table will be changed. If it partially fails, the table could be left without primary key constraint.
-  - You are about to drop the column `UserID` on the `mutes` table. All the data in the column will be lost.
-  - You are about to drop the column `UserID` on the `notifications` table. All the data in the column will be lost.
-  - The primary key for the `trendsinteractions` table will be changed. If it partially fails, the table could be left without primary key constraint.
-  - You are about to drop the column `trendID` on the `trendsinteractions` table. All the data in the column will be lost.
-  - You are about to drop the column `Username` on the `user` table. All the data in the column will be lost.
-  - The primary key for the `views` table will be changed. If it partially fails, the table could be left without primary key constraint.
-  - You are about to drop the column `UserID` on the `views` table. All the data in the column will be lost.
+  - The primary key for the `Blocks` table will be changed. If it partially fails, the table could be left without primary key constraint.
+  - You are about to drop the column `UserID` on the `Blocks` table. All the data in the column will be lost.
+  - You are about to drop the column `User1ID` on the `Conversations` table. All the data in the column will be lost.
+  - You are about to drop the column `User2ID` on the `Conversations` table. All the data in the column will be lost.
+  - The primary key for the `Follow` table will be changed. If it partially fails, the table could be left without primary key constraint.
+  - You are about to drop the column `FollowingUserID` on the `Follow` table. All the data in the column will be lost.
+  - You are about to drop the column `UserID` on the `Follow` table. All the data in the column will be lost.
+  - You are about to drop the column `UserID` on the `Interactions` table. All the data in the column will be lost.
+  - The primary key for the `Likes` table will be changed. If it partially fails, the table could be left without primary key constraint.
+  - You are about to drop the column `UserID` on the `Likes` table. All the data in the column will be lost.
+  - You are about to drop the column `DirectMessagesId` on the `Media` table. All the data in the column will be lost.
+  - You are about to drop the column `InteractionsID` on the `Media` table. All the data in the column will be lost.
+  - The primary key for the `Mentions` table will be changed. If it partially fails, the table could be left without primary key constraint.
+  - You are about to drop the column `UserID` on the `Mentions` table. All the data in the column will be lost.
+  - The primary key for the `Mutes` table will be changed. If it partially fails, the table could be left without primary key constraint.
+  - You are about to drop the column `UserID` on the `Mutes` table. All the data in the column will be lost.
+  - You are about to drop the column `UserID` on the `Notifications` table. All the data in the column will be lost.
+  - The primary key for the `TrendsInteractions` table will be changed. If it partially fails, the table could be left without primary key constraint.
+  - You are about to drop the column `trendID` on the `TrendsInteractions` table. All the data in the column will be lost.
+  - You are about to drop the column `Username` on the `User` table. All the data in the column will be lost.
+  - The primary key for the `Views` table will be changed. If it partially fails, the table could be left without primary key constraint.
+  - You are about to drop the column `UserID` on the `Views` table. All the data in the column will be lost.
   - You are about to drop the `trends` table. If the table is not empty, all the data it contains will be lost.
-  - A unique constraint covering the columns `[username]` on the table `User` will be added. If there are existing duplicate values, this will fail.
-  - Added the required column `userID` to the `Blocks` table without a default value. This is not possible if the table is not empty.
-  - Added the required column `user1ID` to the `Conversations` table without a default value. This is not possible if the table is not empty.
-  - Added the required column `user2ID` to the `Conversations` table without a default value. This is not possible if the table is not empty.
-  - Added the required column `followingUserID` to the `Follow` table without a default value. This is not possible if the table is not empty.
-  - Added the required column `userID` to the `Follow` table without a default value. This is not possible if the table is not empty.
-  - Added the required column `userID` to the `Interactions` table without a default value. This is not possible if the table is not empty.
-  - Added the required column `userID` to the `Likes` table without a default value. This is not possible if the table is not empty.
-  - Added the required column `interactionsID` to the `Media` table without a default value. This is not possible if the table is not empty.
-  - Added the required column `userID` to the `Mentions` table without a default value. This is not possible if the table is not empty.
-  - Added the required column `userID` to the `Mutes` table without a default value. This is not possible if the table is not empty.
-  - Added the required column `userID` to the `Notifications` table without a default value. This is not possible if the table is not empty.
+  - A unique constraint covering the columns `[Username]` on the table `User` will be added. If there are existing duplicate values, this will fail.
+  - Added the required column `UserID` to the `Blocks` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `User1ID` to the `Conversations` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `User2ID` to the `Conversations` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `FollowingUserID` to the `Follow` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `UserID` to the `Follow` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `UserID` to the `Interactions` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `UserID` to the `Likes` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `InteractionsID` to the `Media` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `UserID` to the `Mentions` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `UserID` to the `Mutes` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `UserID` to the `Notifications` table without a default value. This is not possible if the table is not empty.
   - Added the required column `trend` to the `TrendsInteractions` table without a default value. This is not possible if the table is not empty.
-  - Added the required column `username` to the `User` table without a default value. This is not possible if the table is not empty.
-  - Added the required column `userID` to the `Views` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `Username` to the `User` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `UserID` to the `Views` table without a default value. This is not possible if the table is not empty.
 
 */
 
 
 -- DropForeignKey
-ALTER TABLE `trendsinteractions` DROP FOREIGN KEY `TrendsInteractions_trendID_fkey`;
+ALTER TABLE `TrendsInteractions` DROP FOREIGN KEY `TrendsInteractions_trendID_fkey`;
 
 
 -- AlterTable
-ALTER TABLE `trendsinteractions` DROP PRIMARY KEY,
+ALTER TABLE `TrendsInteractions` DROP PRIMARY KEY,
     DROP COLUMN `trendID`,
     ADD COLUMN `trend` VARCHAR(191) NOT NULL,
     ADD PRIMARY KEY (`trend`, `interactionID`);
