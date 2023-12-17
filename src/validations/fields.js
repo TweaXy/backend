09a07e74055cf94fb55519d8f2f326341bdd7320
aliaa gheis <aliaagheis@gmail.com>
2023-12-18 00:34:50 +0200

    [🎨 REF] change validation message of min username char

--- src/validations/fields.js
+++ src/validations/fields.js
@@ -35,7 +35,7 @@ const phoneField = yup
 
 const usernameField = yup
     .string()
-    .min(5, 'username must be at least 4 characters')
+    .min(5, 'username must be at least 5 characters')
     .max(191, 'username must be at most 191 characters')
     .matches(
         /^[a-zA-Z0-9_]+$/,
